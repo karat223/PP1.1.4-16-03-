7725eb6a6b79b44dcca57e5a758fefa8623aabc1
Kira Korelskaia <kiraswav@gmail.com>
2025-02-07 18:12:32 +0100

    Methods of working with JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util util = new Util();
+        UserServiceImpl userImpl = new UserServiceImpl();
+        byte ageBy = 5;
+        byte age2 = 7;
+        User user = new User("Lea","Nemia", ageBy);
+        User user1 = new User("Miu","Miw", age2);
+        User user2 = new User("Fio", "Leri", age2);
+        User user3 = new User("Rea", "Tirey", ageBy);
+
+        try {userImpl.createUsersTable();
+            userImpl.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println(user.toString());
+            userImpl.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+            System.out.println(user1.toString());
+            userImpl.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+            System.out.println(user2.toString());
+            userImpl.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+            System.out.println(user3.toString());
+            //System.out.println(userImpl.getAllUsers());
+            //userImpl.removeUserById(1);
+            //userImpl.cleanUsersTable();
+            //userImpl.dropUsersTable();
+        }
+        catch (Exception e) {e.printStackTrace();}
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
 
 public class Util {
+    String url = "jdbc:postgresql://localhost:8000/virt_health";
+    String user = "postgres";
+    String pas = "KiraWi";
     // реализуйте настройку соеденения с БД
+    public Connection getConnection() {
+        Connection connection = null;
+        // Попытка подключения
+        try {
+            connection = DriverManager.getConnection(url, user, pas);
+            System.out.println("Connection to PostgreSQL established successfully.");
+            // Ваши действия с базой данных
+        } catch (SQLException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+        }
+        return connection;
+    }
 }
+
