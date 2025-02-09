954ce2dd5f316e38ec4490ecc1e70918be6a4486
Aleksandr-Kolpakov <a.s.kolpakov83@gmail.com>
2025-02-09 22:36:52 +0300

    Методы реализованы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userDao = new UserServiceImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Ivan", "Ivanov", (byte) 41);
+        userDao.saveUser("Petr", "Petrov", (byte) 25);
+        userDao.saveUser("Elena", "Lenina", (byte) 38);
+        userDao.saveUser("Vasia", "Alexandrov", (byte) 11);
+
+        System.out.println(userDao.getAllUsers().toString());
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/DBU";
+        String user = "root";
+        String password = "root";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
