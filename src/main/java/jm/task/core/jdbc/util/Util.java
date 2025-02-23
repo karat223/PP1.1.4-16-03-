caf5b12916e7eaa921ed3e0102d913e7d4ad8771
iluhinzoh1 <ilyanovikov007@mail.ru>
2025-02-23 20:42:14 +0300

    добавил решение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/mysqldata";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("ошибка подключения к бд");
+        }
+    }
+    public static void close() {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)){
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
